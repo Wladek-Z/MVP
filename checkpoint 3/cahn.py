94b59afd88701c27fbbb7bf4f7e46c18847d6c0a
Wladek-Z <s2280450@ed.ac.uk>
2026-03-12 12:45:43 +0000

    wrote code for task 5 need to implement argparser

--- checkpoint 3/cahn.py
+++ checkpoint 3/cahn.py
@@ -20,7 +20,7 @@ class CahnHilliard:
         self.dt = dt
         self.dx = 1
         # Set initial state of board to phi_0 plus some small random noise
-        self.phi = (np.ones((L, L)) * phi_0) + np.random.uniform(-0.25, high=0.25, size=(L, L))
+        self.phi = (np.ones((L, L)) * phi_0) + np.random.uniform(-0.2, high=0.2, size=(L, L))
 
     def order_parameter(self, phi):
         """
@@ -78,6 +78,7 @@ class CahnHilliard:
         cbar = plt.colorbar(img, ax=ax)
         cbar.set_ticks([-1, 0, 1])
         cbar.set_ticklabels([-1, 0, 1], fontsize=16)
+        cbar.set_label(r'order parameter $\phi$', size=16)
 
         plt.xticks([])
         plt.yticks([])  
@@ -92,29 +93,95 @@ class CahnHilliard:
         Returns:
             img: figure displaying the (old) system
         """
-        # Run the update procedure
-        self.update()
+        # Run a sweep of the update procedure
+        for i in range(self.sweep):
+            # Copy the current order parameter field
+            phi = self.phi.copy()
+            # Update the order parameter field
+            self.phi = self.order_parameter(phi)
+
         # Clear the figure
         plt.cla()      
         # Update the figure                                                         
         img = plt.imshow(self.phi, cmap='vanimo', vmin=-1, vmax=1) 
 
-        plt.title('Phase Separation', fontsize = 16)
+        plt.title('Phase Separation with Cahn-Hilliard Equation', fontsize = 16)
         plt.xticks([])
         plt.yticks([]) 
         # Return image of board for animation
         return img
     
-    def update(self):
+
+    def free_energy(self, phi):
         """
-        Run a sweep of the Euler algorithm.
+        Calculate the free energy density.
+        
+        Arguments:
+            phi: the current order parameter field
+
+        Returns:
+            the free energy density of the system
         """
-        for _ in range(self.sweep):
-            # Copy the current order parameter field
+        # Calculate gradient squared via CFD
+        dphi2 = 1 / (4 * self.dx**2)\
+                *   ((np.roll(phi, 1, axis=0) - np.roll(phi, -1, axis=0))**2\
+                +    (np.roll(phi, 1, axis=1) - np.roll(phi, -1, axis=1))**2)
+        # Calculate free energy density at each position
+        f = -0.5 * phi**2 + 0.25 * phi**4 + dphi2
+        # return average
+        return np.mean(f)
+    
+    def task5(self):
+        """
+        Collect data for task 5.
+        """
+        file1 = 'cahn_phi_0.0.txt'
+        file2 = 'cahn_phi_0.5.txt'
+
+        self.collect_data(file1, 0)
+        self.collect_data(file2, 0.5)
+        
+    def collect_data(self, filename, phi_0):
+        """
+        Collect data pertaining to the time required to equilibrate the free energy.
+
+        Arguments:
+            filename: file to save the data
+            phi_0: initial value for the order parameter field
+        """
+        # Set tolerance for equilibrium
+        tol = 1e-6
+
+        with open(filename, 'w') as file:
+            file.write("time,f\n")
+            # Initialise the order parameter field
+            self.phi = (np.ones((self.L, self.L)) * phi_0) \
+                       + np.random.uniform(-0.2, high=0.2, size=(self.L, self.L))
+            # Initialise time
+            t = 0
+            # Record initial free energy density
             phi = self.phi.copy()
-            # Update the order parameter field
-            self.phi = self.order_parameter(phi)
+            f = self.free_energy(phi)
+            # Write initial values to file
+            file.write(f"{t},{f}\n")
+            # Run the simulation until equilibrium
+            while True:
+                t += 1
+                phi = self.phi.copy()
+                self.phi = self.order_parameter(phi)
+                f_new = self.free_energy(phi)
+                # Check equilibrium condition
+                if np.abs(f_new - f) < tol:
+                    break
+                else:
+                    f = f_new
+                    file.write(f"{t},{f}\n")
+
+
+        
+
+        
     
 if __name__ == "__main__":
-    CH = CahnHilliard(50, 0, 0.001)
+    CH = CahnHilliard(50, 0, 0.01)
     CH.animation()
